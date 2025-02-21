32237c2c3fd82ac03a4027203c2f8ad2f1bcc980
divy-mohan <divymohan.awgp@gmail.com>
2025-02-21 22:10:35 +0530

    Update settings_rail.py

--- vedmata_web_designing/settings_rail.py
+++ vedmata_web_designing/settings_rail.py
@@ -3,3 +3,7 @@ from vedmata_web_designing.settings import *
 from decouple import config
 
 SECRET_KEY = config('SECRET_KEY')
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-f43a7.up.railway.app',
+]
